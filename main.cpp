5d4a61c96208c8a9b944bc38bd712efc19fb42c3
EashanVytla <Eashan.Vytla@gmail.com>
2023-11-27 13:33:36 -0500

    Test

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ const int w_width = 329;
 const int menu_x_split = 155;
 const int menu_y_split = 115;
 
-const int back_menu_x = 329;
+const int back_menu_x = 309;
 const int back_menu_y = 219;
 
 bool prev_touch = false;
