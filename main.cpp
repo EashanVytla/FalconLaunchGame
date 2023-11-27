c7cb77ec5d1ebc63b97290a5200d54133cf7a4e3
Allen Thomas <allenthom05@gmail.com>
2023-11-27 11:43:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include "FEHLCD.h"
-
+void makeMenue();
 int main()
 {
+    //make the menue from function
+    makeMenue();
+    while (1) {
+        LCD.Update();
+        // Never end
+    }
+    return 0;
+}
+void makeMenue(){
     //Draw the dividing borders of the menue screen
     LCD.SetFontColor(LIGHTGOLDENRODYELLOW);
     LCD.FillRectangle(155,0,10,239);
     LCD.FillRectangle(0,115,319,10);
 
     //Write Launch to the top left section of the menue
-    LCD.WriteAt("Launch",40,50);
+    LCD.WriteAt("Start",40,50);
     //Write leaderboard to the top right section of the menue
-    LCD.WriteAt("Leaderboard",180,50);
+    LCD.WriteAt("Leaderboard",175,50);
     //Write Credits to the bottom left section of the menue
-    LCD.WriteAt("Credits",180,160);
-
-    while (1) {
-        LCD.Update();
-        // Never end
-    }
-    return 0;
+    LCD.WriteAt("Credit",40,170);
+    //Write instructions at the bottom right section of the menue
+    LCD.WriteAt("Instructions",170,170);
 }
