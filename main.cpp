e68409f3ca956b980037fa0f74563672619363af
EashanVytla <Eashan.Vytla@gmail.com>
2023-11-27 12:12:52 -0500

    test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include "FEHLCD.h"
+
 void makeMenue();
+
+const int w_height = 239;
+const int w_width = 329;
+
+//0 - Game
+//1 - Leaderboard
+//2 - Credit
+//3 - Instructions
+int menu_state = 0;
+
 int main()
 {
     //make the menue from function
     makeMenue();
     while (1) {
+        switch(menu_state){
+            case 0:
+                LCD.WriteAt("Play game here", w_height/2, w_width/2);
+        }
+
         LCD.Update();
         // Never end
     }
     return 0;
 }
+
 void makeMenue(){
     //Draw the dividing borders of the menue screen
     LCD.SetFontColor(LIGHTGOLDENRODYELLOW);
