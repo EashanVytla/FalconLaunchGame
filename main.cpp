d1d6826dc60bf17e69dc4f4700b1b86135943a9c
Allen Thomas <allenthom05@gmail.com>
2023-11-27 11:17:25 -0500

    Update main.cpp
    Starting Menue

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include "FEHLCD.h"
-#include "FEHUtility.h"
 
-int main() {
+int main()
+{
+    //Draw the dividing borders of the menue screen
+    LCD.SetFontColor(LIGHTGOLDENRODYELLOW);
+    LCD.FillRectangle(155,0,10,239);
+    LCD.FillRectangle(0,115,319,10);
 
+    //Write Launch to the top left section of the menue
+    LCD.WriteAt("Launch",40,50);
+    //Write leaderboard to the top right section of the menue
+    LCD.WriteAt("Leaderboard",180,50);
+    //Write Credits to the bottom left section of the menue
+    LCD.WriteAt("Leaderboard",180,50);
+
+    while (1) {
+        LCD.Update();
+        // Never end
+    }
     return 0;
 }
