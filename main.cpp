bc3f9cf8ae3df6e036fe9e22fb73832bfa001911
Allen Thomas <allenthom05@gmail.com>
2023-11-30 14:44:25 -0500

    Test

--- main.cpp
+++ main.cpp
@@ -291,7 +291,7 @@ void moveBackgroundUp(int alt){
     float changeInY = .4;
 
     // Check if altitude is between 300 and 500
-    if (alt >= Rocket::buffer_altitude && alt <= Rocket::max_altitude +1) {
+    if (alt >= Rocket::buffer_altitude && alt <= Rocket::max_altitude +50) {
         // Calculate the linear decrease in changeInY as altitude increases from 300 to 500
         // Linearly decrease from 0.35 to 2 over 200 units of altitude
         float changeInY = initialChangeInY + ((Rocket::max_altitude - alt) / 100.0) * (finalChangeInY - initialChangeInY);
