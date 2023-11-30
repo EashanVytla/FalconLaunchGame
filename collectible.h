1694e59acbeb93b950e65fe8dd6890b6f3936d6f
EashanVytla <Eashan.Vytla@gmail.com>
2023-11-30 16:08:59 -0500

    Test

--- collectible.h
+++ collectible.h
@@ -12,8 +12,8 @@ class Collectible{
         void setY(int y);
         virtual void draw() = 0;
         virtual bool collision(int x, int y) = 0;
-        virtual int getWidth();
-        virtual int getHeight();
+        virtual int getWidth() = 0;
+        virtual int getHeight() = 0;
         void move(int dy);
 };
 #endif
