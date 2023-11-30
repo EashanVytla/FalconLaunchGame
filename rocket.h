c38d89b437ac02b0907767fe64ae2285479d16cb
Allen Thomas <allenthom05@gmail.com>
2023-11-30 17:08:35 -0500

    Test

--- rocket.h
+++ rocket.h
@@ -5,7 +5,7 @@ class Rocket{
     public:
         static const int width = 13, height = 80;
         static const int max_altitude = 1000, buffer_altitude = 800;
-        static const float max_up_speed = 2, max_down_speed = 2;
+        static const float max_up_speed = 5, max_down_speed = 2;
         
         Rocket(int x, int y);
         Rocket();
