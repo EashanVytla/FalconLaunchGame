cb645ec90d0cd458db7a18616a814293569619ba
EashanVytla <Eashan.Vytla@gmail.com>
2023-12-01 14:48:10 -0500

    Test

--- collectibles.cpp
+++ collectibles.cpp
@@ -54,6 +54,7 @@ void Collectibles::update(Rocket* rocket){
         if(ptr->getY() < ptr->getHeight()){
             std::cout << "Reached EOS: removing index " << index << std::endl;
             std::cout << "Object Y: " << ptr->getY() << std::endl;
+            std::cout << "Vector Size: " << objects.size() << std::endl;
             remove(index);
             collision = true;
         }
