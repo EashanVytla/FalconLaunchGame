9b90f2ebff0f01f6159260a9a55aad9440a556fb
Allen Thomas <allenthom05@gmail.com>
2023-12-01 12:12:31 -0500

    Test

--- collectibles.cpp
+++ collectibles.cpp
@@ -1,11 +1,10 @@
+
 #include "collectibles.h"
 #include "rocket.h"
 #include <memory>
 #include <FEHRandom.h>
 #include <iostream>
 #include <math.h>
-#include "star.h"
-#include "fuel.h"
 
 Collectibles::Collectibles(){
     generate_rate = 1;
@@ -14,9 +13,10 @@ Collectibles::Collectibles(){
 int prev_time = 0;
 
 void Collectibles::generate(float time, int altitude){
-    //TODO: Make sure this is designed for when the rocket is going down NOT UP
+    //TODO: Make sure this is designed for when the rocket is going down 
     generate_rate = sqrt((1.0 - (float)altitude/Rocket::max_altitude) * 30.0);
     int every = 60 / generate_rate;
+
     //75% of the collectibles will be Fuel and 25% will be Stars
     //Completely random selection process
     int whichCol = Random.RandInt()/24575;
@@ -25,7 +25,7 @@ void Collectibles::generate(float time, int altitude){
 
     if(time - prev_time > every){
         if(whichCol == 0){
-            objects.push_back(std::make_unique<Collectible>());
+            objects.push_back(std::make_unique<Fuel>());
         }else{
             objects.push_back(std::make_unique<Star>());
         }
