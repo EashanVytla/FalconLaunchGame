0ec2444f050d880d71ef896c37c072fe79904e51
Allen Thomas <allenthom05@gmail.com>
2023-12-01 14:40:40 -0500

    Test

--- main.cpp
+++ main.cpp
@@ -227,7 +227,7 @@ int main()
                 }
                 if(landing){
                     launchpad.draw();
-                    while(rocket.getY() < Window::w_height-20 ){
+                    if(rocket.getY() < Window::w_height-100){
                         rocket.moveY(-1);
                     }
                 }
