9714b469ca8cc530b42af7e7d5447d0d1cbdc276
Allen Thomas <allenthom05@gmail.com>
2023-12-01 12:12:37 -0500

    Test

--- collectibles.h
+++ collectibles.h
@@ -6,9 +6,10 @@
 #include "Collectible.h"
 #include "rocket.h"
 #include <memory>
+
 class Collectibles{
     private:
-        std::vector<std::unique_ptr<Collectible> > objects;
+        std::vector<std::unique_ptr<Collectible>> objects;
         int generate_rate; //Collectibles generated per second
 
     public:
