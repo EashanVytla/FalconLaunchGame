f0bfab134002abd9b801392d0bf9ab8a502a3c7d
Allen Thomas <allenthom05@gmail.com>
2023-12-01 11:46:18 -0500

    Test

--- collectibles.h
+++ collectibles.h
@@ -8,7 +8,7 @@
 #include <memory>
 class Collectibles{
     private:
-        std::vector<std::unique_ptr<Collectible>> objects;
+        std::vector<std::unique_ptr<Collectible> > objects;
         int generate_rate; //Collectibles generated per second
 
     public:
