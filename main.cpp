bae4f5697b1d65c55b6f39b4e37b635d5a3b1cb2
EashanVytla <Eashan.Vytla@gmail.com>
2023-12-02 00:53:59 -0500

    Test

--- main.cpp
+++ main.cpp
@@ -247,7 +247,7 @@ int main()
                     break;
                 case 2:
                     //Pre-Land
-                    moveBackgroundUp(rocket.getAltitude(), 0.2);
+                    moveBackgroundUp(rocket.getAltitude(), 0.01);
                     rocket.moveY(Rocket::max_down_speed);
                     if(rocket.getY() <= 1){
                         rocket_state = 3;
