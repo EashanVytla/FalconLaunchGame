0bab6f7648b9780be59c84bec1fba03788d08df0
Allen Thomas <allenthom05@gmail.com>
2023-12-03 20:55:53 -0500

    comments

--- collectible.cpp
+++ collectible.cpp
@@ -13,7 +13,9 @@ int Collectible::getY(){
 }
 
 void Collectible::setRandomPos(){
+    //get a random x value using RandInt
     x = Random.RandInt()/100;
+    //set Y to window height minus collectible height
     y = Window::w_height - getHeight();
 }
 
@@ -26,13 +28,17 @@ void Collectible::setY(int y){
 }
 
 void Collectible::move(int dy){
+    //move the y by subtracting dy from it
     y -= dy;
 }
 
 bool Collectible::collision(int x, int y){
+    //intitialize a bool if the collectible has collided
     bool collided = false;
+    //if the pos is in between between the x and y of the collectible
     if (getX() + getWidth() > x && getX() < x + Rocket::width &&
         getY() + getHeight() > y && getY() < y + Rocket::height) {
+        //set collision to be true
         collided = true;
         std::cout << "Collided" << std::endl;
     }
--- collectible.h
+++ collectible.h
@@ -5,15 +5,52 @@ class Collectible{
         int x;
         int y;
     public:
+
+        /**This function returns the x value of the collectible
+        **/
         int getX();
+
+        /**This function returns the y value of the collectible
+        **/
         int getY();
+
+        /**Sets the position of the collectible to a random number
+        **/
         void setRandomPos();
+
+        /**Sets the x position of the collectible
+        **/
         void setX(int x);
+
+        /**Sets the y position of the collectible
+        **/
         void setY(int y);
+
+        /**draws the collectible to the screen
+         * The function is virtual void so it can be overrided by extending classes
+         * The code was written by: Eashan
+        **/
         virtual void draw() = 0;
+
+        /**checks if the collectible has collided with anything
+         * The returns a boolean and intput an x and y postion as parameters
+         * The code was written by: Allen
+        **/
         bool collision(int x, int y);
+
+        /**This function returns the width of the collectible
+         * The function is virtual void so it can be overrided by extending classes
+        **/
         virtual int getWidth() = 0;
+
+        /**This function returns the height of the collectible
+         * The function is virtual void so it can be overrided by extending classes
+        **/
         virtual int getHeight() = 0;
+
+        /**This function moves the collectible based off the change in Y as the parameter
+         * The function takes a change in y as the paramter
+        **/
         void move(int dy);
 };
 #endif
